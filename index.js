cfed5ae7b65bd46b3221c0b9d97f111321a2233a
Marielbamar21 <marielbamaldonado20@gmail.com>
2023-10-20 14:14:14 -0400

    eliminacion de debug

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ import {pausa, message} from './helpers/message.js';
 console.clear();
 
 const main = async() => {
-    message();
-    pausa();
+    let opt = '';
+    do{
+        opt = await message(); // Menu
+
+        //option config
+        if (opt == '1')
+        {
+            console.log('Hola 1');
+        }
+        else
+        {
+            console.log('otros');
+        }
+
+
+        await pausa();// Pausa
+    }while( opt !== '0')
+    
 }
 main();
